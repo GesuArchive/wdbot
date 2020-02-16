89bbbeb3899c061fae9bd2df996ce9a2fc8902ed
Gesugao-san <28023014+Gesugao-san@users.noreply.github.com>
2020-02-16 23:18:31 +0300

    На пути к завершению модернизации

--- h.js
+++ h.js
@@ -16,6 +16,7 @@ const chokidar	= require('chokidar');
 
 console.log("Importing modules done. Trying to load config files.");
 const cfg	= JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+//const os_commands	= require('./path_config.js');
 
 console.log("Configs loaded, help command: " + cfg.general.cmd_prefix + cfg.commands.general.help + ". Trying to load localization files.");
 if ((cfg.general.OUTPUT_LANGUAGE == "ENG") || (typeof(cfg.general.OUTPUT_LANGUAGE) != String))  {
@@ -67,10 +68,11 @@ async function checkOnline(server) {
 	client.channels.get(cfg.channels_id.COMMAND_LINE).setTopic(`${cfg.servers.first.Discord_show_name}: ${s1_onlinestatus} | ${cfg.servers.second.Discord_show_name}: ${s2_onlinestatus}`);
 };
 
-chokidar.watch(cfg.directories.DEMOS, {ignoreInitial: true, interval: 15000}).on('addDir', (event, path) => { // every 15 seconds
-	if(DEMOS_DIR === "") return;
-	endround_channel.send(`Round №${event.slice(-4)} ended. Replay: https://hub.station13.ru/replay/?roundid=${event.slice(-4)}`); // here is the channel where links to replays posted
-});
+if ((cfg.general.replays_avaliable) && (cfg.directories.DEMOS != "")) {
+	chokidar.watch(cfg.directories.DEMOS, {ignoreInitial: true, interval: 15000}).on('addDir', (event, path) => { // every 15 seconds
+		endround_channel.send(`${lang.endround_message}${event.slice(-4)} ${lang.endround_message2}${event.slice(-4)}`); // here is the channel where links to replays posted
+	});
+}
 
 client.on('message', message => {
 	if (message.author.bot) return;
@@ -79,12 +81,12 @@ client.on('message', message => {
 	if (!message.content.startsWith(cfg.general.cmd_prefix)) return;
 
 	if (message.content.startsWith(cfg.general.cmd_prefix + cfg.commands.general.help)) {
-		console.log("Сommand received: \"help\".");
+		if (cfg.script_debug) console.log(lang.cmd_recived_help);
 		print_help();
 		return;
 	};
 
-	if (message.content.startsWith(cfg.general.cmd_prefix + cfg.commands.general.whoisadmin) && message.author.id == HOST) {
+	if (message.content.startsWith(cfg.general.cmd_prefix + cfg.commands.general.whoisadmin) && message.author.id == cfg.general.HOST_USER_ID) {
 		var msg = message.content.slice(cfg.general.cmd_prefix.length).split(' ');
 		switch(msg[1]) {
 			case Server1.name:
@@ -94,12 +96,12 @@ client.on('message', message => {
 				client.channels.get(cfg.channels_id.COMMAND_LINE).send(`${Server2.admins}`);
 				break;
 			default:
-				client.channels.get(cfg.channels_id.COMMAND_LINE).send(`Run ${cfg.general.cmd_prefix}shelp`);
+				client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.run_help_for_help);
 				return;
 		}
 	};
 
-	if (message.content.startsWith(cfg.general.cmd_prefix + cfg.commands.general.adduser) && message.author.id == HOST) {
+	if (message.content.startsWith(cfg.general.cmd_prefix + cfg.commands.general.adduser) && message.author.id == cfg.general.HOST_USER_ID) {
 		var msg = message.content.slice(cfg.general.cmd_prefix.length).split(' ');
 		switch(msg[1]) {
 			case Server1.name:
@@ -107,9 +109,9 @@ client.on('message', message => {
 				if (uid == -1) {
 					Server1.admins.push(msg[2])
 					fs.writeFileSync('./s1.json', JSON.stringify(Server1, null, 4));
-					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`Added <@${msg[2]}> to ${msg[1]}.`);
+					client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.contoller_added_to_server+msg[2]+lang.contoller_added_to_server2+msg[1]+lang.contoller_added_to_server3);
 				} else {
-					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`User <@${msg[2]}> already in ${msg[1]}.`);
+					client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.contoller_already_added+msg[2]+lang.contoller_already_added2+msg[1]+lang.contoller_already_added3);
 				}
 				break;
 			case Server2.name:
@@ -117,18 +119,18 @@ client.on('message', message => {
 				if (uid == -1) {
 					Server2.admins.push(msg[2])
 					fs.writeFileSync('./s2.json', JSON.stringify(Server2, null, 4));
-					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`Added <@${msg[2]}> to ${msg[1]}.`);
+					client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.contoller_added_to_server+msg[2]+lang.contoller_added_to_server2+msg[1]+lang.contoller_added_to_server3);
 				} else {
-					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`User <@${msg[2]}> already in ${msg[1]}.`);
+					client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.contoller_already_added+msg[2]+lang.contoller_already_added2+msg[1]+lang.contoller_already_added3);
 				}
 				break;
 			default:
-				client.channels.get(cfg.channels_id.COMMAND_LINE).send(`Run ${cfg.general.cmd_prefix}shelp`);
+				client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.run_help_for_help);
 				return;
 		}
 	};
 
-	if (message.content.startsWith(cfg.general.cmd_prefix + cfg.commands.general.remuser) && message.author.id == HOST) {
+	if (message.content.startsWith(cfg.general.cmd_prefix + cfg.commands.general.remuser) && message.author.id == cfg.general.HOST_USER_ID) {
 		var msg = message.content.slice(cfg.general.cmd_prefix.length).split(' ');
 		switch(msg[1]) {
 			case Server1.name:
@@ -136,9 +138,9 @@ client.on('message', message => {
 				if (uid != -1) {
 					Server1.admins.splice(Server1.admins.indexOf(msg[2]), 1);
 					fs.writeFileSync('./s1.json', JSON.stringify(Server1, null, 4));
-					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`Removed <@${msg[2]}> from ${msg[1]}.`);
+					client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.contoller_removed+msg[2]+lang.contoller_removed2+msg[1]+lang.contoller_removed3);
 				} else {
-					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`No such ID <@${msg[2]}> in ${msg[1]}.`);
+					client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.contoller_to_remove_not_found+msg[2]+lang.contoller_to_remove_not_found2+msg[1]+lang.contoller_to_remove_not_found3);
 				};
 				break;
 			case Server2.name:
@@ -146,13 +148,13 @@ client.on('message', message => {
 				if (uid != -1) {
 					S22.admins.splice(Server2.admins.indexOf(msg[2]), 1);
 					fs.writeFileSync('./s2.json', JSON.stringify(Server2, null, 4));
-					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`Removed <@${msg[2]}> from ${msg[1]}.`);
+					client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.contoller_removed+msg[2]+lang.contoller_removed2+msg[1]+lang.contoller_removed3);
 				} else {
-					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`No such ID <@${msg[2]}> in ${msg[1]}.`);
+					client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.contoller_to_remove_not_found+msg[2]+lang.contoller_to_remove_not_found2+msg[1]+lang.contoller_to_remove_not_found3);
 				}
 				break;
 			default:
-				client.channels.get(cfg.channels_id.COMMAND_LINE).send(`Run ${cfg.general.cmd_prefix}shelp`);
+				client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.run_help_for_help);
 				return;
 		}
 	};
@@ -188,14 +190,14 @@ async function issue_command(uid, cmd, server) {
 			port   = Server2.port;
 			break;
 		default:
-			client.channels.get(cfg.channels_id.COMMAND_LINE).send(`Run ${cfg.general.cmd_prefix}shelp`);
+			client.channels.get(cfg.channels_id.COMMAND_LINE).send(lang.run_help_for_help);
 			return;
 	};
 	if (admins.includes(uid)) {
 		if (devs.includes(uid)) {
 			switch (cmd) {
 				case cfg.commands.build_control.deploy:
-					shell.exec('sh ' + cfg.directories.REPOS + 'repo_' + sname + '/tools/deploy.sh ' + cfg.directories.PROD + 'server_' + sname, { silent: true });
+					shell.exec(`sh ${cfg.directories.REPOS}repo_${sname}/tools/deploy.sh ${cfg.directories.REPOS}server_${sname}`, { silent: true });
 					client.channels.get(cfg.channels_id.COMMAND_LINE).send(`${sname}: Deployed.`);
 					break;
 				case cfg.commands.build_control.compile:
@@ -248,7 +250,7 @@ async function issue_command(uid, cmd, server) {
 };
 
 async function print_help() {
-	console.log("Function \"print_help\" called.");
+	if (cfg.script_debug) console.log("Function \"print_help\" called.");
 	var h = "Help contents:\n";
 	h += `> Host user privileges:\n`;
 	h += `\`${cfg.general.cmd_prefix}${cfg.commands.general.adduser} SERVER_NAME UID\` - adds user to server\n`;
