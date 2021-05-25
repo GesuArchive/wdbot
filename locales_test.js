3eb199a5a7534d440b0c60c80e2bdf9ad45455db
Gesugao-san <gesugao.san@gmail.com>
2021-05-25 16:12:25 +0300

    ok no resx only json

--- locales_test.js
+++ locales_test.js
@@ -1,49 +1,9 @@
 
-// npm install resx-parser --save
-// https://www.npmjs.com/package/resx-parser
-// "./locales/test.resx"
-
-const fs = require('fs');
 console.log("Locales test program started.");
-//fs.readFile('./locales/test.resx', 'utf8', callback);
 
-// https://stackoverflow.com/a/32873155
-fs.readFile("./locales/test.resx", 'utf8', function(err, data) {
-  if (!err) {
-    //console.log(data);
-  }
-});
+var fs = require('fs');
+var locale = JSON.parse(fs.readFileSync('./locales/ru_RU.json', 'utf8'));
 
-
-//
-//const fs = require('fs');
-const convert = require('xml-js');
-const xmlFile = fs.readFileSync("./locales/test.resx", 'utf8');
-const jsonData = JSON.parse(convert.xml2json(xmlFile, {compact: true, spaces: 4}));
-
-
-// All
-console.log(jsonData.root.data);  // outputs: {_text: 'Test Value 2'}
-
-
-// Loop
-/*
-for (let key in jsonData.root.data) {
-  console.log('Entry ' + key + '/' + (Object.keys(jsonData.root.data).length - 1) + ', value: ' + Object.values(jsonData.root.data[key].value)[0])
-}
-*/
-
-
-// Find
-/*
-const targetNode =
-
-    jsonData.root.data
-    .find(x =>
-
-        x._attributes.name === 'Test Key 2'
-    );
-
-console.log(Object.values(targetNode.value)[0]);  // outputs: {_text: 'Test Value 2'}
-*/
+console.log("locale: " + locale);
+console.log("locale.general: " + locale.general);
 
