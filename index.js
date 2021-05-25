fe89ca0f3021417ba9c62fdc20dca0f80995e19f
Gesugao-san <gesugao.san@gmail.com>
2021-05-25 18:03:33 +0300

    Are you ready, kids?

--- index.js
+++ index.js
@@ -93,11 +93,11 @@ const cfg = JSON.parse(fs.readFileSync(os_config_path, 'utf8'));
 console.log(`[${stat_msg.ok}] Configs loaded, help command: ${cfg.general.cmd_prefix}${cfg.commands.general.help}. Trying to load localization files...`);
 if ((cfg.general.OUTPUT_LANGUAGE == "ENG") || (typeof(cfg.general.OUTPUT_LANGUAGE) != String))  {
   console.log(`[${stat_msg.load}] According to configuration file, trying to loading file of language: English...`);
-  const lang = require(cfg.directories.LOC_ENG);
+  var lang = JSON.parse(fs.readFileSync(cfg.directories.LOC_ENG, 'utf8'));
   console.log(`[${stat_msg.ok}] Localization file file required.`);
 } else {
   console.log(`[${stat_msg.loading}] According to configuration, trying to loading file of language: Russian...`);
-  const lang = require(cfg.directories.LOC_RUS);
+  var lang = JSON.parse(fs.readFileSync(cfg.directories.LOC_RUS, 'utf8'));
   console.log(`[${stat_msg.ok}] Localization file file required.`);
 };
 
