bcc75ac31450cd0eb68b66de656e6b2f68e9c59e
Gesugao-san <gesugao.san@gmail.com>
2021-05-25 16:23:14 +0300

    What about hjson?

--- locales_test.js
+++ locales_test.js
@@ -7,3 +7,5 @@ var locale = JSON.parse(fs.readFileSync('./locales/ru_RU.json', 'utf8'));
 console.log("locale: " + locale);
 console.log("locale.general: " + locale.general);
 
+// https://support.crowdin.com/supported-formats/
+// ToDo: https://www.npmjs.com/package/i18next
